989eac97a6fc721c88658f5148c4a0a2fa17a1b8
phillipsrr <34726361+phillipsrr@users.noreply.github.com>
2025-05-25 16:26:10 -0500

    Add .next/server/pages/api/estados.js 1748208370548

---
+++ .next/server/pages/api/estados.js
@@ -0,0 +1,5 @@
+const CHUNK_PUBLIC_PATH = "server/pages/api/estados.js";
+const runtime = require("../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/[root-of-the-server]__fe7998d0._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/src/pages/api/estados.ts [api] (ecmascript)\" } [api] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/src/pages/api/estados.ts [api] (ecmascript)\" } [api] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
