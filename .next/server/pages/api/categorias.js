22126d4d014d09c1f9acf7846473105f91b680ed
phillipsrr <34726361+phillipsrr@users.noreply.github.com>
2025-05-25 16:26:12 -0500

    Add .next/server/pages/api/categorias.js 1748208372420

---
+++ .next/server/pages/api/categorias.js
@@ -0,0 +1,5 @@
+const CHUNK_PUBLIC_PATH = "server/pages/api/categorias.js";
+const runtime = require("../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/[root-of-the-server]__8c11fd1b._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/src/pages/api/categorias.ts [api] (ecmascript)\" } [api] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/src/pages/api/categorias.ts [api] (ecmascript)\" } [api] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
